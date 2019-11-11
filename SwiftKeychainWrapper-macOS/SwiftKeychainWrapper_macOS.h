fe1e403ffdbd97bff322177e8213ddfe2297a2f0
Marc Charbonneau <marc@mbcharbonneau.com>
2019-11-11 13:11:24 -0800

    Add macOS target.

---
+++ SwiftKeychainWrapper-macOS/SwiftKeychainWrapper_macOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftKeychainWrapper_macOS.h
+//  SwiftKeychainWrapper-macOS
+//
+//  Created by Marc Charbonneau on 11/11/19.
+//  Copyright © 2019 Jason Rendel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftKeychainWrapper_macOS.
+FOUNDATION_EXPORT double SwiftKeychainWrapper_macOSVersionNumber;
+
+//! Project version string for SwiftKeychainWrapper_macOS.
+FOUNDATION_EXPORT const unsigned char SwiftKeychainWrapper_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftKeychainWrapper_macOS/PublicHeader.h>
+
+
